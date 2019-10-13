76a52c09440266bfb0c2fee2f89e97f82d1b29e4
Navratan <navratan@limeroad.com>
2019-10-13 22:27:06 +0530

    Added config class

---
+++ app/src/main/java/com/nuffwritten/swipablecards/utils/Config.java
@@ -0,0 +1,12 @@
+package com.nuffwritten.swipablecards.utils;
+
+/**
+ * Created by navratan on 2019-10-13
+ */
+public class Config {
+
+    public String[] getActions() {
+        return new String[] {"Delete, Add, Remove"};
+    }
+
+}
